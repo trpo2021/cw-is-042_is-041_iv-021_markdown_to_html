4960ab353473c977ea7c7fb603cfd0ecfd2cd608
Artem <love-gura@yandex.ru>
2021-04-14 17:52:13 +0700

    Added flag handling

---
+++ src/interface.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if (strcmp("-f", argv[1]) == 0 && strcmp("-f", argv[3]) == 0)
+    {
+        func_1(argv[2], argv[4]);
+    }
+    else if (strcmp("-d", argv[1]) == 0 && strcmp("-f", argv[3] == 0))
+    {
+        func_2(stdin, argv[4]);
+    }
+    else if (strcmp("-d", argv[1]) == 0 && strcmp("-d", argv[3]) == 0)
+    {
+        func_3(stdin, stdout);
+    }
+    else if (strcmp("-help", argv[1]) == 0)
+    {
+        printf("Usage:\n\tconverter -f <in-file-name> -f <out-file-name> (The input and output data are files)\n\tconverter -d stdin -f <out-file-name> (The input data is read from the keyboard, the output data is a file)\n\tconverter -d stdin -d stdout (Input data is read from the keyboard, output data is output to the terminal)\n\tconverter -help (Call help)");
+    }
+}
