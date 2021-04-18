a7a4c68ed7cddf14a7e539b028740adbd05a752d
Artem <love-gura@yandex.ru>
2021-04-18 23:29:52 +0700

    Update reading and writing functions

--- src/ReadAndOutput.c
+++ src/ReadAndOutput.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *read_line()
+{
+    printf("Print \"-quit\" to exit input field");
+    char *buf = malloc(10000);
+    while (strcmp("-quit\n", stdin) != 0)
+    {
+        scanf("%s", &buf);
+    }
+    return buf;
+}
+
 char *read_file(const char *file_in)
 {
     FILE *input = fopen(file_in, "r");
