debe7c8e6791f1d48de99b0c177decc431581990
Artem <love-gura@yandex.ru>
2021-04-19 18:06:00 +0700

    Update hash table creating

---
+++ src/internals/taglist/taglist.h
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define HASHTAB_SIZE 100
+
+typedef struct tag Tag;
+
+struct tag
+{
+    char key[100];
+    char value[100];
+    Tag* next;
+};
+
+typedef struct
+{
+    Tag tags[100];
+} TagList;
+
+unsigned int hashtab_hash(char* key);
+void hashtab_init(Tag** hashtab);
+void hashtab_add(Tag** hashtab, char* key, char* value);
+void create_taglist(Tag** map);
