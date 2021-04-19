1cd999e960e94834abe383d386abfb36144f06d1
Artem <love-gura@yandex.ru>
2021-04-19 20:36:52 +0700

    Include functions prototypes

--- src/internals/taglist/taglist.h
+++ src/internals/taglist/taglist.h
@@ -22,3 +22,5 @@ unsigned int hashtab_hash(char* key);
 void hashtab_init(Tag** hashtab);
 void hashtab_add(Tag** hashtab, char* key, char* value);
 void create_taglist(Tag** map);
+Tag* hashtab_lookup(Tag** hashtab, char* key);
+void hashtab_delete(Tag** hashtab, char* key);
