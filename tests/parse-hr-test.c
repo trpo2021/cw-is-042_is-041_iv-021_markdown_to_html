1e06a31ee3af5c23855e9836ae3d49cc2de509e0
Westflow <repllimate@gmail.com>
2021-05-20 23:56:44 +0700

    Update rule-performer tests Произведен небольшой рефакторинг кода тестов

--- tests/parse-hr-test.c
+++ tests/parse-hr-test.c
@@ -1,23 +1,30 @@
 #include "parse-single-rule-helper.h"
 #include <ctest.h>
 
-/* in case when we have correct rule ( 3 <= x ('*' | '_')) */
-CTEST(parse_hr_rule, correct)
+static TNode* create_hr_from_test_data(char* raw_data)
 {
-    srand(time(NULL));
-    Array(Token) arr = create_test_data(generate_sequence_of_terms((char[]){'*', '_'}, 2, rand() % 100 + 3));
-    TNode* exp = init_tnode(NodeHorizontalLine, create_string("<hr/>"), NULL, false);
+    Array(Token) arr = create_test_data(raw_data);
 
     RulePerformer perf = {0};
     init_performer(&perf, arr, 0);
 
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* result = perf.invoke(&perf, perf.count);
+    free_test_data(arr);
+
+    return result;
+}
+
+/* in case when we have correct rule ( 3 <= x ('*' | '_')) */
+CTEST(parse_hr_rule, correct)
+{
+    TNode* exp = init_tnode(NodeHorizontalLine, create_string("<hr/>"), NULL, false);
+
+    TNode* real = create_hr_from_test_data(generate_sequence_of_terms((char[]){'*', '_'}, 2, rand() % 100 + 3));
 
     ASSERT_EQUAL(exp->type, real->type);
     ASSERT_STR(exp->head->text(exp->head), real->head->text(real->head));
     ASSERT_FALSE(real->nesting);
 
-    free_test_data(arr);
     free_tnode(exp);
     free_tnode(real);
 }
@@ -25,7 +32,6 @@ CTEST(parse_hr_rule, correct)
 /* in case when we have spaces between tokens */
 CTEST(parse_hr_rule, correct_with_spaces)
 {
-    srand(time(NULL));
     String* str = create_string("   _   *   _ ***   ____    ___________ ***** ***\n");
     Array(Token) arr = tokenize(str);
     TNode* exp = init_tnode(NodeHorizontalLine, create_string("<hr/>"), NULL, false);
@@ -48,18 +54,12 @@ CTEST(parse_hr_rule, correct_with_spaces)
 /* in case when we have fewer tokens than allowed */
 CTEST(parse_hr_rule, incorrect_count_less)
 {
-    srand(time(NULL));
-    Array(Token) arr = create_test_data(generate_sequence_of_terms((char[]){'*', '_', ' '}, 3, rand() % 2 + 1));
     TNode* exp = init_tnode(NodeHorizontalLine, create_string("<hr/>"), NULL, false);
 
-    RulePerformer perf = {0};
-    init_performer(&perf, arr, skip_spaces(0, arr));
-
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* real = create_hr_from_test_data(generate_sequence_of_terms((char[]){'*', '_', ' '}, 3, rand() % 2 + 1));
 
     ASSERT_NOT_EQUAL(exp->type, real->type);
 
-    free_test_data(arr);
     free_tnode(exp);
     free_tnode(real);
 }
@@ -67,19 +67,13 @@ CTEST(parse_hr_rule, incorrect_count_less)
 /* in case when we have any other token in line */
 CTEST(parse_hr_rule, incorrect_with_others_token)
 {
-    srand(time(NULL));
     const char tokens[] = "\n_*`=-+<#![> ]()";
-    Array(Token) arr = create_test_data(generate_sequence_of_terms(tokens, sizeof(tokens), rand() % 100 + 3));
     TNode* exp = init_tnode(NodeHorizontalLine, create_string("<hr/>"), NULL, false);
 
-    RulePerformer perf = {0};
-    init_performer(&perf, arr, skip_spaces(0, arr));
-
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* real = create_hr_from_test_data(generate_sequence_of_terms(tokens, sizeof(tokens), rand() % 100 + 3));
 
     ASSERT_NOT_EQUAL(exp->type, real->type);
 
-    free_test_data(arr);
     free_tnode(exp);
     free_tnode(real);
 }
--- tests/parse-hunderline-test.c
+++ tests/parse-hunderline-test.c
@@ -1,24 +1,30 @@
 #include "parse-single-rule-helper.h"
 #include <ctest.h>
 
-/* in case when we have correct rule ( 2 <= x ('=')) */
-CTEST(parse_header_underline_rule, h1_correct)
+static TNode* create_header_from_test_data(char* raw_data)
 {
-    srand(time(NULL));
-    char* raw_data = generate_sequence_of_terms((char[]){'='}, 1, rand() % 100 + 2);
-    TNode* exp = init_tnode(NodeHeadingUnderline, create_string("<h1>"), create_string(raw_data), true);
     Array(Token) arr = create_test_data(raw_data);
 
     RulePerformer perf = {0};
     init_performer(&perf, arr, 0);
 
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* result = perf.invoke(&perf, perf.count);
+
+    free_test_data(arr);
+    return result;
+}
+
+/* in case when we have correct rule ( 2 <= x ('=')) */
+CTEST(parse_header_underline_rule, h1_correct)
+{
+    char* raw_data = generate_sequence_of_terms((char[]){'='}, 1, rand() % 100 + 2);
+    TNode* exp = init_tnode(NodeHeadingUnderline, create_string("<h1>"), create_string(raw_data), true);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_EQUAL(exp->type, real->type);
     ASSERT_STR(exp->head->text(exp->head), real->head->text(real->head));
     ASSERT_STR(exp->content->text(exp->content), real->content->text(real->content));
 
-    free_test_data(arr);
     free_tnode(exp);
     free_tnode(real);
 }
@@ -26,77 +32,60 @@ CTEST(parse_header_underline_rule, h1_correct)
 /* in case when we have correct rule ( 2 <= x ('-')) */
 CTEST(parse_header_underline_rule, h2_correct)
 {
-    srand(time(NULL));
     char* raw_data = generate_sequence_of_terms((char[]){'-'}, 1, rand() % 100 + 2);
     TNode* exp = init_tnode(NodeHeadingUnderline, create_string("<h2>"), create_string(raw_data), true);
-    Array(Token) arr = create_test_data(raw_data);
-
-    RulePerformer perf = {0};
-    init_performer(&perf, arr, 0);
-
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_EQUAL(exp->type, real->type);
     ASSERT_STR(exp->head->text(exp->head), real->head->text(real->head));
     ASSERT_STR(exp->content->text(exp->content), real->content->text(real->content));
 
-    free_test_data(arr);
     free_tnode(exp);
     free_tnode(real);
 }
 
 /* in case when we have fewer tokens than allowed */
-CTEST(parse_header_underline_rule, incorrect_count_less)
+CTEST(parse_header_underline_rule, h1_incorrect_count_less)
 {
-    srand(time(NULL));
-    Array(Token) arr = create_test_data(generate_sequence_of_terms((char[]){'='}, 1, 1));
+    char* raw_data = generate_sequence_of_terms((char[]){'='}, 1, 1);
 
-    RulePerformer perf = {0};
-    init_performer(&perf, arr, 0);
-
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_NOT_EQUAL(NodeHeadingUnderline, real->type);
 
-    free_test_data(arr);
     free_tnode(real);
+}
 
-    arr = create_test_data(generate_sequence_of_terms((char[]){'-'}, 1, 1));
+CTEST(parse_header_underline_rule, h2_incorrect_count_less)
+{
+    char* raw_data = generate_sequence_of_terms((char[]){'-'}, 1, 1);
 
-    init_performer(&perf, arr, 0);
-
-    real = perf.invoke(&perf, perf.count);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_NOT_EQUAL(NodeHeadingUnderline, real->type);
 
-    free_test_data(arr);
     free_tnode(real);
 }
 
 /* in case when we have spaces between tokens */
-CTEST(parse_header_underline_rule, incorrect_spaces_between)
+CTEST(parse_header_underline_rule, h1_incorrect_spaces_between)
 {
-    srand(time(NULL));
-    Array(Token) arr = create_test_data(generate_sequence_of_terms((char[]){'=', ' '}, 2, rand() % 10000 + 500));
+    char* raw_data = generate_sequence_of_terms((char[]){'=', ' '}, 2, rand() % 10000 + 500);
 
-    RulePerformer perf = {0};
-    init_performer(&perf, arr, skip_spaces(0, arr));
-
-    TNode* real = perf.invoke(&perf, perf.count);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_NOT_EQUAL(NodeHeadingUnderline, real->type);
 
-    free_test_data(arr);
     free_tnode(real);
+}
 
-    arr = create_test_data(generate_sequence_of_terms((char[]){'-', ' '}, 2, rand() % 10000 + 500));
+CTEST(parse_header_underline_rule, h2_incorrect_spaces_between)
+{
+    char* raw_data = generate_sequence_of_terms((char[]){'-', ' '}, 2, rand() % 10000 + 500);
 
-    init_performer(&perf, arr, 0);
-
-    real = perf.invoke(&perf, perf.count);
+    TNode* real = create_header_from_test_data(raw_data);
 
     ASSERT_NOT_EQUAL(NodeHeadingUnderline, real->type);
 
-    free_test_data(arr);
     free_tnode(real);
 }
